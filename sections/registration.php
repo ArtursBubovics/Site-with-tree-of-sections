cb3e11a5558945642cb19c06a63b5c520cb0164b
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-03 20:55:42 +0300

    Created a registration page

---
+++ sections/registration.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main page</title>
+    <link rel="stylesheet" href="../assets/css/main_settings/min-reset.css">
+    <link rel="stylesheet" href="../assets/css/main_settings/main.css">
+    <link rel="stylesheet" href="../assets/css/authentication.css">
+    <link rel="stylesheet" href="../assets/css/input_fields/input_fields.css">
+
+</head>
+
+<body class="authentication-block-container">
+    <div class="authentication-block registration-part">
+        <div class="authentication-block_inner-container">
+            <div class="authentication-block_another-page">
+                <p class="authentication-block_additional-text">Do you already have an account?</p>
+                <button class="authentication-block_additional-text authentication-block_link">Login</button>
+            </div>
+            <h3 class="main-header">Registration</h3>
+            <form class="authentication-block_form" action="login-action.php" method="post">
+                <div class="authentication-block_form-container">
+                    <div class="authentication-block_form-inputs_block">
+                        <input class="input_field" type="text" name="username" placeholder="Name" required>
+                        <input class="input_field" type="password" name="password" placeholder="Password" required>
+                        <input class="input_field" type="password" name="confirm_password" placeholder="Confirm Password" required>
+                    </div>
+                    <div class="authentication-block_form-button-block">
+                        <button class="authentication-block_form-button" type="submit">Register</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</body>
+
+</html>
