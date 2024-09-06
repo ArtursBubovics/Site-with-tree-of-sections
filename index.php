797597e360dca8f6a2295179f5b8fd0fec5cb21e
ArtursBubovics <artursbubovocs@gmail.com>
2024-09-06 13:23:08 +0300

    Improved the authorization structure

--- index.php
+++ index.php
@@ -7,28 +7,28 @@
     <title>Main page</title>
     <link rel="stylesheet" href="./assets/css/main_settings/min-reset.css">
     <link rel="stylesheet" href="./assets/css/main_settings/main.css">
-    <link rel="stylesheet" href="./assets/css/authentication.css">
+    <link rel="stylesheet" href="./assets/css/authentication_login.css">
     <link rel="stylesheet" href="./assets/css/input_fields/input_fields.css">
     <script src="assets/js/authentication/redirect_to_another_page/login/login.js" defer></script>
     <script src="./assets/js/authentication/authorization_forms/login_form.js" defer></script>
 </head>
 
-<body class="authentication-block-container">
-    <div class="authentication-block">
-        <div class="authentication-block_inner-container">
-            <div class="authentication-block_another-page">
-                <p class="authentication-block_additional-text">Haven't registered yet?</p>
-                <button class="authentication-block_additional-text authentication-block_link" id="authentication-to-registration-btn">Registration</button>
+<body class="authentication-block-login-container">
+    <div class="authentication-block-login">
+        <div class="authentication-block-login_inner-container">
+            <div class="authentication-block-login_another-page">
+                <p class="authentication-block-login_additional-text">Haven't registered yet?</p>
+                <button class="authentication-block-login_additional-text authentication-block-login_link" id="authentication-to-registration-btn">Registration</button>
             </div>
             <h3 class="main-header">Login</h3>
-            <form class="authentication-block_form" id="loginForm" method="post">
-                <div class="authentication-block_form-container">
-                    <div class="authentication-block_form-inputs_block">
+            <form class="authentication-block-login_form" id="loginForm" method="post">
+                <div class="authentication-block-login_form-container">
+                    <div class="authentication-block-login_form-inputs_block">
                         <input class="input_field" type="text" name="username" placeholder="Name" minlength="6" required>
                         <input class="input_field" type="password" name="password" placeholder="Password" minlength="6" required>
                     </div>
-                    <div class="authentication-block_form-button-block">
-                        <button class="authentication-block_form-button" type="submit">Login</button>
+                    <div class="authentication-block-login_form-button-block">
+                        <button class="authentication-block-login_form-button" type="submit">Login</button>
                     </div>
                 </div>
             </form>
--- sections/registration.php
+++ sections/registration.php
@@ -7,29 +7,29 @@
     <title>Main page</title>
     <link rel="stylesheet" href="../assets/css/main_settings/min-reset.css">
     <link rel="stylesheet" href="../assets/css/main_settings/main.css">
-    <link rel="stylesheet" href="../assets/css/authentication.css">
+    <link rel="stylesheet" href="../assets/css/authentication_registration.css">
     <link rel="stylesheet" href="../assets/css/input_fields/input_fields.css">
     <script src="../assets/js/authentication/redirect_to_another_page/registration/registration.js" defer></script>
     <script src="../assets/js/authentication/authorization_forms/registration_form.js" defer></script>
 </head>
 
-<body class="authentication-block-container">
-    <div class="authentication-block registration-part">
-        <div class="authentication-block_inner-container">
-            <div class="authentication-block_another-page">
-                <p class="authentication-block_additional-text">Do you already have an account?</p>
-                <button class="authentication-block_additional-text authentication-block_link" id="authentication-to-login-btn">Login</button>
+<body class="authentication-block-registration-container">
+    <div class="authentication-block-registration registration-part">
+        <div class="authentication-block-registration_inner-container">
+            <div class="authentication-block-registration_another-page">
+                <p class="authentication-block-registration_additional-text">Do you already have an account?</p>
+                <button class="authentication-block-registration_additional-text authentication-block-registration_link" id="authentication-to-login-btn">Login</button>
             </div>
             <h3 class="main-header">Registration</h3>
-            <form class="authentication-block_form" id="registrationForm" method="post">
-                <div class="authentication-block_form-container">
-                    <div class="authentication-block_form-inputs_block">
+            <form class="authentication-block-registration_form" id="registrationForm" method="post">
+                <div class="authentication-block-registration_form-container">
+                    <div class="authentication-block-registration_form-inputs_block">
                         <input class="input_field" type="text" name="username" placeholder="Name" minlength="6" required>
                         <input class="input_field" type="password" name="password" placeholder="Password" minlength="6" required>
                         <input class="input_field" type="password" name="confirm_password" placeholder="Confirm Password" minlength="6" required>
                     </div>
-                    <div class="authentication-block_form-button-block">
-                        <button class="authentication-block_form-button" type="submit">Register</button>
+                    <div class="authentication-block-registration_form-button-block">
+                        <button class="authentication-block-registration_form-button" type="submit">Register</button>
                     </div>
                 </div>
             </form>
